3be684e65a6585f75f78f73c7b6c6284f2482f30
Ken Kester <Ken@KenKester.com>
2018-05-17 08:32:16 -0700

    Rider Monitor

---
+++ ridermon.ino
@@ -0,0 +1,101 @@
+
+unsigned long timeMilli = 0;
+unsigned long lastEventMilli = 0;
+unsigned long lastToneStart = 0;
+unsigned long sensorCount = 0; 
+unsigned long rfidCount = 0;
+unsigned long totalRfidCount = 0; 
+unsigned long totalSensorCount = 0;
+
+#define SENSORPIN  2
+#define GREEN 3
+#define RED 4 
+#define YELLOW 5
+#define TONE 6
+
+const unsigned long timeoutMillis = 3000;
+
+const unsigned long toneDurationMillis = 250;
+
+void setup() {
+  // setup the Interrupt listener for the beam break 
+  attachInterrupt(digitalPinToInterrupt(SENSORPIN), onPinInterrupt, FALLING);  // TODO: Might match mode to beam break sensor
+  // set up listening on the serial port  ( we do the serial port monitoring in the serialEvent function )
+  Serial.begin(9600);
+
+  // setup the pins
+  pinMode( GREEN, OUTPUT );
+  pinMode( RED, OUTPUT ); 
+  pinMode( YELLOW, OUTPUT );
+  pinMode( TONE, OUTPUT );
+  
+}
+
+void loop() {
+  watchMillis();
+  updateIndicatorPins;
+  monitorTimeout();
+}
+
+void onPinInterrupt(){
+  lastEventMilli = timeMilli;
+  sensorCount++;
+  totalSensorCount++;
+}
+
+void onRFIDEvent(){ 
+  lastEventMilli = timeMilli; 
+  rfidCount++;
+  totalRfidCount++;
+  lastToneStart=timeMilli;
+}
+
+void serialEvent(){
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') {
+      onRFIDEvent();
+    }
+  }
+}
+
+void watchMillis(){
+  timeMilli = millis(); 
+}
+
+void updateIndicatorPins(){ 
+    if (rfidCount < sensorCount ) { 
+      digitalWrite( GREEN, LOW );
+      digitalWrite( RED, HIGH );
+      digitalWrite( YELLOW, LOW );
+    } else if (rfidCount > sensorCount ) { 
+      digitalWrite( GREEN, LOW );
+      digitalWrite( RED, LOW );
+      digitalWrite( YELLOW, HIGH );
+    } else { 
+      digitalWrite( GREEN, HIGH );
+      digitalWrite( RED, LOW );
+      digitalWrite( YELLOW, LOW );
+    }
+
+    if( timeMilli- lastToneStart > toneDurationMillis ) { 
+       digitalWrite( TONE, LOW );
+    }else{
+      digitalWrite( TONE, HIGH );
+    }
+}
+
+void monitorTimeout(){
+   if( (timeMilli - lastEventMilli) > timeoutMillis ){
+      sensorCount=0;
+      rfidCount=0;
+      // output a bit of json with the totals 
+      Serial.write( "{\"RFIDEvents\":" );
+      Serial.write( totalRfidCount );
+      Serial.write(",\"SensorEvent\":");
+      Serial.write( totalSensorCount);
+      Serial.write( "}\n" );
+      Serial.flush();
+   }
+}
+
